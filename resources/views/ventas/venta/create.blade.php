43ca6fab976a6c43b37e0de404ad1a8895bb638e
TheDeivit <da.059@hotmail.com>
2020-05-18 02:55:38 -0500

    Proyecto-Tienda'

--- resources/views/ventas/venta/create.blade.php
+++ resources/views/ventas/venta/create.blade.php
@@ -2,7 +2,7 @@
 @section ('contenido')
     <div class="row">
         <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-            <h3>Nuevo Ingreso</h3>
+            <h3>Nueva Venta</h3>
             @if (count($errors)>0)
             <div class="alert alert-danger">
                 <ul>
@@ -15,14 +15,14 @@
         </div>
     </div>
 
-            {!!Form::open(array('url'=>'compras/ingreso','method'=>'POST','autocomplete'=>'off'))!!}
+            {!!Form::open(array('url'=>'ventas/venta','method'=>'POST','autocomplete'=>'off'))!!}
             {{Form::token()}}
     <div class="row">
         
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
             <div class="form-group">
-                <label for="proveedor">Proveedor</label>
-                <select name="idproveedor" id="idproveedor" class="form-control selectpicker" data-live-search="true">
+                <label for="cliente">Cliente</label>
+                <select name="idcliente" id="idcliente" class="form-control selectpicker" data-live-search="true">
                     @foreach($personas as $persona)
                     <option value="{{$persona->idpersona}}">{{$persona->nombre}}</option>
                     @endforeach
@@ -64,7 +64,7 @@
                         <label for="idarticulo">Articulo</label>
                         <select name="pidarticulo" class="form-control selectpicker"  id="pidarticulo" data-live-search="true">
                             @foreach($articulos as $articulo)
-                            <option value="{{$articulo->idarticulo}}">{{$articulo->articulo}}</option>
+                            <option value="{{$articulo->idarticulo}}_{{$articulo->stock}}_{{$articulo->precio_promedio}}">{{$articulo->articulo}}</option>
                             @endforeach
                         </select>
                     </div>
@@ -79,15 +79,22 @@
 
         <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
             <div class="form-group">
-                <label for="precio_compra">Precio Compra</label>
-                    <input type="number" name="pprecio_compra" id="pprecio_compra" class="form-control" placeholder="Precio Compra...">
+                <label for="stock">Stock</label>
+                    <input type="number" disabled name="pstock" id="pstock" class="form-control" placeholder="Stock...">
             </div>
         </div>
 
         <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
             <div class="form-group">
                 <label for="precio_venta">Precio Venta</label>
-                    <input type="number" name="pprecio_venta" id="pprecio_venta" class="form-control" placeholder="Precio Venta...">
+                    <input type="number" disabled name="pprecio_venta" id="pprecio_venta" class="form-control" placeholder="Precio Venta...">
+            </div>
+        </div>
+
+        <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
+            <div class="form-group">
+                <label for="descuento">Descuento</label>
+                    <input type="number" name="pdescuento" id="pdescuento" class="form-control" placeholder="Descuento...">
             </div>
         </div>
 
@@ -103,8 +110,8 @@
                     <th>Opciones</th>
                     <th>Articulo</th>
                     <th>Cantidad</th>
-                    <th>Precio Compra</th>
                     <th>Precio Venta</th>
+                    <th>Descuento</th>
                     <th>Subtotal</th>
                 </thead>
                 <tfoot>
@@ -113,7 +120,7 @@
                     <th></th>
                     <th></th>
                     <th></th>
-                    <th><h4 id="total"> $/.0.00</h4></th>
+                    <th><h4 id="total"> $/.0.00</h4><input type="hidden" name="total_venta" id="total_venta"></th>
                 </tfoot>
             </table>
         </div>
@@ -144,17 +151,32 @@
     total=0;
     subtotal=[];
     $("#guardar").hide();
+    $("#pidarticulo").change(mostrarValores);
+
+    function mostrarValores()
+    {
+        datosArticulo=document.getElementById('pidarticulo').value.split('_');
+        $("#pprecio_venta").val(datosArticulo[2]);
+        $("#pstock").val(datosArticulo[1]);
+    }
 
     function agregar()
     {
-        idarticulo=$("#pidarticulo").val();
+        datosArticulo=document.getElementById('pidarticulo').value.split('_');
+
+
+        idarticulo=datosArticulo[0];
         articulo=$("#pidarticulo option:selected").text();
         cantidad=$("#pidarticulo").val();
-        precio_compra=$("#pprecio_compra").val();
+        descuento=$("#pdescuento").val();
         precio_venta=$("#pprecio_venta").val();
+        stock=$("#pstock").val();
 
-        if (idarticulo != "" && cantidad!="" && precio_compra!="" && precio_venta!="")
+        if (idarticulo != "" && cantidad!="" && cantidad>0 && descuento!="" && precio_venta!="")
         {
+            if(stock>=cantidad)
+            {
+
             subtotal[cont]=(cantidad*precio_compra);
             total=total+subtotal[cont];
 
@@ -165,6 +187,8 @@
             $("#total").html("$/. "+total);
             evaluar();
             $("#detalles").append(fila);
+                            
+            }
         }
         else
         {
